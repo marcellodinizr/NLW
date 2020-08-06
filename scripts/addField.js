8afc9427c9e6bcaaa03e0f139e8927e7d92efa12
marcellodinizr <marcellodinizrocha@gmail.com>
2020-08-06 10:24:51 -0300

    day03 finished

---
+++ scripts/addField.js
@@ -0,0 +1,20 @@
+
+/* Ao clicar em "+ Novo Horário" (add time), ir duplicando 
+ os horarios disponiveis (schedule-item) abaixo, 
+ porem limpando os dados dentro deles! */
+
+
+document.querySelector('#add-time').addEventListener('click', cloneField);
+
+function cloneField() {
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+
+    const fields = newFieldContainer.querySelectorAll('input');
+
+    fields.forEach(function (field) {
+        console.log(field);
+        field.value = "";
+    })
+
+    document.querySelector('#schedule-items').appendChild(newFieldContainer);
+}
