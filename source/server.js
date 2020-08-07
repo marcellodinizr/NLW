7f162f71f440e0cc36c567687991d3a9696db865
marcellodinizr <marcellodinizrocha@gmail.com>
2020-08-07 09:08:09 -0300

    finished day04

--- source/server.js
+++ source/server.js
@@ -1,25 +1,25 @@
 const proffys = [
     {
         name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "88887777",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        avatar: "https://github.com/diego3g.png",
+        whatsapp: "900000000",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
         cost: "20",
-        weekyday: [0],
+        weekday: [0],
         time_from: [720],
-        time_to: [1220]
+        time_to: [1200]
     },
     {
-        name: "Daniele Evangelista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "88887777",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        name: "Mayk Brito",
+        avatar: "https://github.com/maykbrito.png",
+        whatsapp: "900000000",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
         cost: "20",
-        weekyday: [1],
+        weekday: [1],
         time_from: [720],
-        time_to: [1220]
+        time_to: [1200]
     }
 ]
 
@@ -32,37 +32,64 @@ const subjects = [
     "Geografia",
     "História",
     "Matemática",
-    "Português",
-    "Química",
+    "Portugês",
+    "Química"
 ]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubjects(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
 
 function pageLanding(req, res) {
     return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.render("study.html")
+    const filters = req.query
+
+
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+
+    const isNotEmpty = Object.keys(data).length != 0
+
+    if (isNotEmpty) {
+
+        data.subject = getSubjects(data.subject)
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+
+    return res.render("give-classes.html", { subjects, weekdays })
 }
 
-const express = require('express')
-const server = express()
+const express = require('express');
+const server = express();
 
 
-//configurar nunjucks
-const nunjucks = require('nunjucks')
+const nunjucks = require('nunjucks');
 nunjucks.configure('source/views', {
     express: server,
     noCache: true,
 })
-server
-    // configurar arquivos estáticos(css, scripts, imagens)
-    .use(express.static("public"))
-    // rotas da aplicação
+
+server.use(express.static("public"))
     .get("/", pageLanding)
     .get("/study", pageStudy)
     .get("/give-classes", pageGiveClasses)
-    .listen(5500)
+    .listen(5500);
