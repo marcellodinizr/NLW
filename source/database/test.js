819c394d2057aeec047c214c831ccb7d24160890
marcellodinizr <marcellodinizrocha@gmail.com>
2020-08-07 23:43:16 -0300

    Next Level Week

--- source/database/test.js
+++ source/database/test.js
@@ -13,7 +13,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Química",
+        subject: 1,
         cost: "20",
     }
 
@@ -30,6 +30,37 @@ Database.then(async (db) => {
         }
     ]
 
-    await createProffy(db, { proffyValue, classValue, classScheduleValues })
+    // await createProffy(db, { proffyValue, classValue, classScheduleValues })
 
+    // Consultar os dados inseridos
+
+    // Todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    //console.log(selectedProffys)
+
+    // Consultar as classes de um determinado proffesor
+    // e trazer junto os dados do proffesor
+
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    // console.log(selectClassesAndProffys)
+
+    // O horário que a pessoa trabalha por exemplo
+    // é das 8h - 18h , o horário do time_from (8h)
+    // precisa ser antes ou igual ao horário solicitado
+    // o time_to precisa ser acima
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "420"
+        AND class_schedule.time_to > "1300"
+    `)
+
+    console.log(selectClassesSchedules)
 })
--- source/server.js
+++ source/server.js
@@ -1,95 +1,31 @@
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar: "https://github.com/diego3g.png",
-        whatsapp: "900000000",
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1200]
-    },
-    {
-        name: "Mayk Brito",
-        avatar: "https://github.com/maykbrito.png",
-        whatsapp: "900000000",
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1200]
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Portugês",
-    "Química"
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-function getSubjects(subjectNumber) {
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-
-
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length != 0
-
-    if (isNotEmpty) {
-
-        data.subject = getSubjects(data.subject)
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
+// Servidor
 const express = require('express');
 const server = express();
 
+const { 
+    pageLanding, 
+    pageStudy, 
+    pageGiveClasses,
+    saveClasses    
+} = require('./pages')
 
+// Configurar nunjucks (template engine)    
 const nunjucks = require('nunjucks');
 nunjucks.configure('source/views', {
     express: server,
     noCache: true,
 })
 
-server.use(express.static("public"))
+
+// Inicio e configuração do servidor
+server
+// Receber os dados do req-body
+.use(express.urlencoded({ extended: true }))
+// Configurar arquivos estáticos(css, scripts, imagens)
+.use(express.static("public"))
     .get("/", pageLanding)
     .get("/study", pageStudy)
     .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
+    // Start do servidor
     .listen(5500);
